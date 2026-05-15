22ad5bdee997e86ba17d0484df9f4de3f6890647
Neon64makoto <neon64phone@gmail.com>
2026-05-14 20:27:47 -0500

    Initialize game canvas and animation loop

---
+++ game.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+let x = 100;
+
+function loop() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+
+  ctx.fillStyle = "cyan";
+  ctx.fillRect(x,200,50,50);
+
+  x++;
+
+  requestAnimationFrame(loop);
+}
+
+loop();
